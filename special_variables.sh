b44c241e1f62827ffdc7240ea6e4012d49346216
sairajeshd <>
2022-12-25 23:43:15 +0530

    using special variables

--- special_variables.sh
+++ special_variables.sh
@@ -5,8 +5,13 @@
 # $1-$9 command line variables we can pass the variable through the command script
 #Ex: sh test.sh 100 200 300
 #here $1 = 100 ,$2 = 200,$3 = 300
+#$* prints all the variables used in the script
+#$# ,$@ both are same --- prints the number of variables used in the script
+
 echo "The name of the script is $0"
 a=$1
 b=$2
 echo "the value of a  is $a"
 echo "the value of b is $b"
+echo "the variables used in the script are $*"
+echo "the number of variables used in the script are $#"
