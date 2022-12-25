01199ea4b83cdebfdb37d0eae68f3a1dc0bab427
sairajeshd <>
2022-12-25 22:29:14 +0530

    updated the code for different colours

--- colors.sh
+++ colors.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#once you enable the colour code we should close that using \e[0m otherwise the colour code will continue
+# priting the forground colour
 echo -e "\e[32m i am printing green colour \e[0m"
 echo -e "\e[31m i am printing red colour \e[0m"
-echo -e "\e[34m i am printing yellow colour \e[0m"
+echo -e "\e[33m i am printing yellow colour \e[0m"
 echo -e "\e[34m i am printing blue colour \e[0m"
+
+#printing the foreground and background colour
+echo "\e[41;33m printing the background coloour nad text in foreground colour \e[0m"
+
