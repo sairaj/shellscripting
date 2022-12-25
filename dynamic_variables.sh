edd0caf9e99bb7fd06fd985801e06d39db537427
sairajeshd <>
2022-12-25 23:24:49 +0530

    using expressions

--- dynamic_variables.sh
+++ dynamic_variables.sh
@@ -3,4 +3,4 @@
 #declaring the expressions
 DATE=$(date +%F)
 echo "Today's date is $DATE"
-echo "the number of users currently logged in are $(who | wc -l)"
+echo "the number of users currently logged in are -e [\e 31m $(who | wc -l)\e[0ms"
