c9cb7d0513d3583e87d6c67ebd2071de0621fa3f
sairajeshd <>
2022-12-25 20:49:48 +0530

    Added the first command

---
+++ sample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#shibang notation tells the interpreter whats the code is and baed on that it will undrstand the code and execute
+#we can have only 1 shibang notation and eveything starting with # will be treated as comment
+
+echo "Hello World !"
