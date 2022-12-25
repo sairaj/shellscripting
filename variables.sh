99b21c50a63ab69cba1deb1ec2afe93cf93799f8
sairajeshd <>
2022-12-25 22:48:47 +0530

    updated the code for the variables

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bsh
+#variables are accessed using the $ symbol
+#If you dont declare a value and if you try to print it it will print the null
+a=10
+b=20
+c=30
+d=40
+echo "The value of a is $a"
+echo "the value of b is {a}"
+echo "The value of e is $e"
