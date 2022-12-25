2dd1a0b2fc4b39233e01fa4f3991e05b7b3aa1ad
sairajeshd <>
2022-12-25 21:03:19 +0530

    updated the code

--- sample.sh
+++ sample.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #shibang notation tells the interpreter whats the code is and baed on that it will undrstand the code and execute
 #we can have only 1 shibang notation and eveything starting with # will be treated as comment
-
+#Escape sequence characters
 echo "Hello World !"
 echo "Good morning"
 echo "Welcome to devops class"
+echo line1\nline2
