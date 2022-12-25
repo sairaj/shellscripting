8fa49b7a48cd872d05928a3095e8df46be417985
sairajeshd <>
2022-12-25 22:22:56 +0530

    updated the code for different colours

--- colors.sh
+++ colors.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo -e "\e[32m i am printing green colour \e[0m"
+echo -e "\e[31m i am printing red colour \e[0m"
+echo -e "\e[34m i am printing yellow colour \e[0m"
+echo -e "\e[34m i am printing blue colour \e[0m"
