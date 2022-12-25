9542583da0143b3525ee97c4e3c3f35c2e435e97
sairajeshd <>
2022-12-25 22:50:37 +0530

    updated the code for the variables

--- variables.sh
+++ variables.sh
@@ -1,6 +1,7 @@
 #!/bin/bsh
 #variables are accessed using the $ symbol
 #If you dont declare a value and if you try to print it it will print the null
+#we can pass the varaible to the heap memeory using export keyword
 a=10
 b=20
 c=30
