6cb37b697000d4ebf0efaa35c3c445acd524c0ef
sairajeshd <>
2022-12-26 22:41:48 +0530

    added functions new1

--- 08-function.sh
+++ 08-function.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Declaring a sample function
-# ( paranthesis )
-sample(){
-    echo "I am a sample function"
-    echo "If you want to call me, just type sample"
-    echo "sample function is completed"
-}
+# # Declaring a sample function
+# # ( paranthesis )
+# sample(){
+#     echo "I am a sample function"
+#     echo "If you want to call me, just type sample"
+#     echo "sample function is completed"
+# }
 
-# calling the function
-sample
-
+# # calling the function
+# sample
+echo "Already uptodate:"
 stat(){
     echo "Number of logged sessions: $(who | wc -l)"
 
