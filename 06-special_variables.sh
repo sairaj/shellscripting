64e1f99b09f4898eac4f517550da6f99faa16204
sairajeshd <>
2023-01-07 11:59:01 +0530

    test9

--- 06-special_variables.sh
+++ 06-special_variables.sh
@@ -10,7 +10,7 @@
 # $?       : Shows the exit code of the previous command  
 # $1 to $9 : command line variables ; ex : sh test.sh  100 200 300 
 # $* or $@ : Prints all the variables used in this script
-# $#       : Prints the number variables used in this script
+# $#       : Prints the number of variables used in this script
 # In linux apart from A to Z or a to z or 0 to  9 are special characters 
 a=$1 
 b=$2 
