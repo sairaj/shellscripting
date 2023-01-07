17746f9ab4e9433fb6bcf7caae927b3a28c0355f
sairajeshd <>
2023-01-07 16:50:48 +0530

    loops2

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-for list in val1 val2 val3 val4 val5 ; do
-    echo Value is $i
+for list1 in val1 val2 val3 val4 val5 ; do
+    echo Value is $list1
 done
 
-
+for list in cart user frontent mongodb redis rabbitmq ; do
+    echo value is $list
+done
 # while loop : a conditional loop   
-
-val=0
-while [ $val -lt 10 ]; do
-  echo $val
-  i=$(($val+1))
-done
+<<COMMENT
+    Below code not working
+    val=0
+    while [ $val -lt 10 ]; do
+    echo I = $val
+    i=$(($val+1))
+    done
+COMMENT
