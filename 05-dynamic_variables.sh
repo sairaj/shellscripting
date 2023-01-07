4552ff4fd62c2a9e72a6cb0ee0f4ddd62b1932b2
sairajeshd <>
2023-01-07 11:34:59 +0530

    test7

--- 05-dynamic_variables.sh
+++ 05-dynamic_variables.sh
@@ -2,6 +2,15 @@
 # dynamic variables 
 #declaring the expressions
 # expressions should be enclosed in paranthesis
-DATE=$(date +%F)
-echo "Today's date is $DATE"
-echo  -e "the number of users currently logged in are \e[31m $(who | wc -l)\e[0m"
+
+# DATE="24-12-2022"   # HardCoding the value
+DATE=$(date +%F)      # Deeclaring expression to the variable
+echo -e "Good Morning, today's date i \e[32m  $DATE  \e[0m"
+echo -e "Total number of opened sessions is \e[32m $(who | wc -l) \e[0m"
+echo -e "todays date is : $(date)"
+
+# Always fetch the information dynamically for the variable which keep on changing, hardcoding creates complexity
+
+# () : Paranthesis      : Expressions should be enclused in paranthesis 
+# {} : Flower Brackets
+# [] : Square Brackets 
