bd97f8df24904af4eab631e24636fd2eeb8a9080
sairajeshd <>
2023-01-07 15:53:50 +0530

    testing2

--- 08-function.sh
+++ 08-function.sh
@@ -10,7 +10,6 @@ sample() {
 
 # # calling the function
 # sample 
-echo " load average : $(uptime | awk -F : '{print $1, $2, $3, $NF}')"
 
 stat() {
     echo "Number of opened sessions : $(who | wc -l)" 
