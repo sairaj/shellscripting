4a603b6ef1623728eb792ae9da7908f19a7554ba
sairajeshd <>
2023-01-07 16:41:31 +0530

    loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -3,3 +3,12 @@
 for list in val1 val2 val3 val4 val5 ; do
     echo Value is $i
 done
+
+
+# while loop : a conditional loop   
+
+val=0
+while [ $val -lt 10 ]; do
+  echo I = $val
+  i=$($val+1)
+done
