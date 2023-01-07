ca9c5f3002af3948e6bdd1fed0ea1e3622ce7f76
sairajeshd <>
2023-01-07 10:55:54 +0530

    test4

--- 03-colors.sh
+++ 03-colors.sh
@@ -9,3 +9,7 @@ echo -e "\e[34m i am printing blue colour \e[0m"
 #printing the foreground and background colour
 echo  -e "\e[41;33m printing the background coloour and text in foreground colour \e[0m"
 
+# How to print background color ?
+
+echo -e "\e[41;33m Welcome to colors \e[0m "
+echo -e "\e[47;32m Avatar2 is a very good file \e[0m "
