e763a39d9e76dcbf8582e0a7ea90a4985679985e
sairajeshd <>
2023-01-07 16:22:18 +0530

    if

--- 12-if.sh
+++ 12-if.sh
@@ -18,12 +18,23 @@
 
 c=$1
 
-if [ "$c" -eq "10" ]; then
-    echo "value is c 10 "
-elif [ "$c" -eq "20" ]; then
-    echo "value of c is 20"
-elif [ "$c" -eq "30" ]; then
-    echo "value of c is 30"
-else    
-    echo "value is not 10 or 20 or 30"
+# if [ "$c" -eq "10" ]; then
+#     echo "value is c 10 "
+# elif [ "$c" -eq "20" ]; then
+#     echo "value of c is 20"
+# elif [ "$c" -eq "30" ]; then
+#     echo "value of c is 30"
+# else    
+#     echo "value is not 10 or 20 or 30"
+# fi
+
+
+if [ "$c" -lt "10"]; then
+    echo "Value is less than 10"
+elif [ "$c" -gt "10" && "$c" -lt "20" ]; then
+    echo "value is between 10 and 20"
+elif [ "$c" -gt "20" && "$c" -lt "30" ]; then
+    echo "Value is between 20 and 30"
+else
+    echo "Value is greater than 30"
 fi
